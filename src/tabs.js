f7fea17ce68dcfb679279e4ac005c7c2ba1fca86
Alex Perez <alejandro.gperez745@gmail.com>
2024-05-17 14:54:46 -0500

    menu update  - visible on screen

--- src/tabs.js
+++ src/tabs.js
@@ -1,37 +1,17 @@
-// const tabList = document.querySelector(".tabList");
-
-async function getAllTabs(tabsBodyId) {
-  const body = document.querySelector(`#${tabsBodyId}`);
-  const tabs = await chrome.tabs.query({ currentWindow: true });
-
-  const list = tabs.map((tab) => {
-    return `<div class="__TABS_Tab">${tab.title}</div>`;
-  });
-
-  body.innerHTML += list.join("");
-}
-
-async function getTabId() {
-  const [tab] = await chrome.tabs.query({
-    active: true,
-    lastFocusedWindow: true,
-  });
-  return tab.id;
-}
-
 //inject script
-async function tabScript() {
-  const TabsBodyId = "__TABS_Body";
+async function tabScript(tabList) {
   const body = document.querySelector("body");
-  const tabMenuContainer = `<main id="${TabsBodyId}"><div>div1</div><div>div2</div></main>`;
-  // await getAllTabs(TabsBodyId);
+  const tabMenuContainer = `
+<main id="__TABS_Body">
+  <section id="__TABS_List">${tabList}</section>
+</main>`;
 
   body.innerHTML += tabMenuContainer;
-  console.log("THIS IS FROM THE SCRIPT!");
 }
 
 async function loadScript() {
   const tabId = await getTabId();
+  const tabList = await getAllTabs();
 
   // JS + CSS injection
   chrome.scripting.insertCSS(
@@ -46,11 +26,29 @@ async function loadScript() {
         .executeScript({
           target: { tabId: tabId },
           func: tabScript,
+          args: [tabList],
         })
         .then(() => console.log("script loaded"));
     },
   );
 }
 
+async function getAllTabs() {
+  const tabs = await chrome.tabs.query({ currentWindow: true });
+
+  const list = tabs.map((tab) => {
+    return `<div id="__TABS_Tab">${tab.title}</div>`;
+  });
+
+  return list.join("");
+}
+
+async function getTabId() {
+  const [tab] = await chrome.tabs.query({
+    active: true,
+    lastFocusedWindow: true,
+  });
+  return tab.id;
+}
+
 loadScript();
-// getAllTabs();
