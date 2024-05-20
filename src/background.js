4203e7003ba19ecd75172d264bdb6551726190b5
Alex Perez <alejandro.gperez745@gmail.com>
2024-05-20 15:56:43 -0500

    FIX - message channel closing early

--- src/background.js
+++ src/background.js
@@ -1,5 +1,70 @@
-function backgroundScript() {
-  console.log("background script");
+async function getAllTabs() {
+  const tabs = await chrome.tabs.query({ currentWindow: true });
+
+  const list = tabs.map((tab) => {
+    return `<div id="__TABS_Tab">${tab.title}</div>`;
+  });
+
+  return list.join("");
 }
 
-backgroundScript();
+async function getTabId() {
+  const [tab] = await chrome.tabs.query({
+    active: true,
+    lastFocusedWindow: true,
+  });
+  return tab.id;
+}
+
+//Generates HTML
+async function tabScript(tabList) {
+  const body = document.querySelector("body");
+  const tabMenuContainer = `
+<main id="__TABS_Body">
+  <section id="__TABS_List">${tabList}</section>
+</main>`;
+
+  body.innerHTML += tabMenuContainer;
+}
+
+// API -> listens for message to send down HTML
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.action === "loadScript") {
+    loadScript(sender, sendResponse);
+  }
+  if (request.action === "removeScript") {
+    console.log("yerr");
+  } else {
+    sendResponse({ success: false, error: "unknown message type" });
+  }
+  return true;
+});
+
+async function loadScript(sender, sendResponse) {
+  try {
+    const tabId = await getTabId();
+    const tabList = await getAllTabs();
+
+    // JS + CSS injection
+    chrome.scripting.insertCSS(
+      {
+        target: {
+          tabId: tabId,
+        },
+        files: ["assets/styles.css"],
+      },
+      () => {
+        chrome.scripting
+          .executeScript({
+            target: { tabId: tabId },
+            func: tabScript,
+            args: [tabList],
+          })
+          .then(() => console.log("script loaded successfully\n", sender));
+      },
+    );
+    sendResponse({ success: true });
+  } catch (error) {
+    sendResponse({ success: false, error: error.message });
+  }
+}
--- src/tabs.js
+++ src/tabs.js
@@ -1,54 +1,33 @@
-//inject script
-async function tabScript(tabList) {
-  const body = document.querySelector("body");
-  const tabMenuContainer = `
-<main id="__TABS_Body">
-  <section id="__TABS_List">${tabList}</section>
-</main>`;
+const body = document.querySelector("body");
 
-  body.innerHTML += tabMenuContainer;
-}
-
-async function loadScript() {
-  const tabId = await getTabId();
-  const tabList = await getAllTabs();
-
-  // JS + CSS injection
-  chrome.scripting.insertCSS(
-    {
-      target: {
-        tabId: tabId,
-      },
-      files: ["assets/styles.css"],
-    },
-    () => {
-      chrome.scripting
-        .executeScript({
-          target: { tabId: tabId },
-          func: tabScript,
-          args: [tabList],
-        })
-        .then(() => console.log("script loaded"));
-    },
-  );
-}
-
-async function getAllTabs() {
-  const tabs = await chrome.tabs.query({ currentWindow: true });
-
-  const list = tabs.map((tab) => {
-    return `<div id="__TABS_Tab">${tab.title}</div>`;
+function loadScript() {
+  chrome.runtime.sendMessage({ action: "loadScript" }, (response) => {
+    if (response && !response.success) {
+      console.log(response.error);
+    } else {
+      console.log("script loaded successfully");
+    }
   });
-
-  return list.join("");
 }
 
-async function getTabId() {
-  const [tab] = await chrome.tabs.query({
-    active: true,
-    lastFocusedWindow: true,
+function removeScript() {
+  chrome.runtime.sendMessage({ action: "removeScript" }, (response) => {
+    if (response && !response.success) {
+      console.log(response.error);
+      return;
+    }
+
+    console.log("removed script successfully");
   });
-  return tab.id;
 }
 
-loadScript();
+body.addEventListener("keydown", (e) => {
+  if (e.ctrlKey && e.key === "n") {
+    loadScript();
+  }
+  if (e.key === "Escape") {
+    removeScript();
+  }
+
+  console.log(e);
+});
